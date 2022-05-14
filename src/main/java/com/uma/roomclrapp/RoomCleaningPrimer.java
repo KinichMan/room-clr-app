f10d3308b2db3c3d910f4989dfe3676c31c949ef
KinichMan <kinichman@gmail.com>
2022-05-13 19:46:47 -0500

    Implementing a message producer

--- src/main/java/com/uma/roomclrapp/RoomCleaningPrimer.java
+++ src/main/java/com/uma/roomclrapp/RoomCleaningPrimer.java
@@ -5,10 +5,19 @@
  */
 package com.uma.roomclrapp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uma.roomwebapp.Room;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,11 +28,22 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class RoomCleaningPrimer implements CommandLineRunner {
 
-    private RestTemplate restTemplate;
+    @Value("amqp.queue.name")
+    private String queueName;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RoomCleaningPrimer.class);
 
-    public RoomCleaningPrimer() {
+    private RestTemplate restTemplate;
+    private final RabbitTemplate rabbitTemplate;
+    private final ConfigurableApplicationContext context;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public RoomCleaningPrimer(RabbitTemplate rabbitTemplate, ConfigurableApplicationContext context, ObjectMapper objectMapper) {
         super();
         this.restTemplate = new RestTemplate();
+        this.rabbitTemplate = rabbitTemplate;
+        this.context = context;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -31,7 +51,16 @@ public class RoomCleaningPrimer implements CommandLineRunner {
         String url = "http://localhost:8080/api/rooms";
         Room[] roomArray = this.restTemplate.getForObject(url, Room[].class);
         List<Room> rooms = Arrays.asList(roomArray);
-        rooms.forEach(System.out::println);
+        rooms.forEach(room -> {
+            LOGGER.info("Sending Message");
+            try {
+                String jsonString = objectMapper.writeValueAsString(room);
+                rabbitTemplate.convertAndSend(queueName, jsonString);
+            } catch (Exception ex) {
+                LOGGER.error("Parsing Exception", ex);
+            }
+        });
+        context.close();
     }
 
 }
